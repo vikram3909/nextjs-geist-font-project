11c331f87415a3a6ae92ef35e05a780c6c336725
Vikram More <84568560+vikram3909@users.noreply.github.com>
2025-05-22 03:52:11 +0530

    Add ITAssetManagement.API/Controllers/BaseApiController.cs 1747866131078

---
+++ ITAssetManagement.API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITAssetManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseApiController : ControllerBase
+{
+    protected ActionResult<T> HandleResult<T>(T result)
+    {
+        if (result == null)
+            return NotFound();
+
+        return Ok(result);
+    }
+}
