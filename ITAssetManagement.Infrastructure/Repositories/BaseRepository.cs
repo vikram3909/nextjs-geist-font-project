88a9762fad251a432f7e3afd566bc546e9f30613
Vikram More <84568560+vikram3909@users.noreply.github.com>
2025-05-22 03:51:22 +0530

    Add ITAssetManagement.Infrastructure/Repositories/BaseRepository.cs 1747866081806

---
+++ ITAssetManagement.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+using ITAssetManagement.Core.Entities;
+using ITAssetManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITAssetManagement.Infrastructure.Repositories;
+
+public abstract class BaseRepository<T> where T : BaseEntity
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    protected BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        entity.UpdatedAt = DateTime.UtcNow;
+        _dbSet.Update(entity);
+    }
+
+    public virtual async Task DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
