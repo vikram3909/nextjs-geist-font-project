f743c9c3f5ab792cb8d84c00a18f17bc3681b053
Vikram More <84568560+vikram3909@users.noreply.github.com>
2025-05-22 03:51:52 +0530

    Add ITAssetManagement.Core/Entities/BaseEntity.cs 1747866111621

---
+++ ITAssetManagement.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace ITAssetManagement.Core.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+}
